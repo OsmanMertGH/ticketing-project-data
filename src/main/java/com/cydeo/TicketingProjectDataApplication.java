2379f4eb4509eeec2b3da5e12c8e0989c29b6bed
Osman MERT <osman_mrt@yahoo.com>
2023-09-26 17:15:02 +0300

    DB and Connection With UI is created!

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,21 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 
-@SpringBootApplication
+@SpringBootApplication //this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
+    //I am trying to add bean in the container thru @Bean annotation
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 }
